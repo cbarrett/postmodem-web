3c2d7fabeba1c46dc8ef8c95b7c7c9e79650f7b0
Colin Barrett <colin@springsandstruts.com>
2014-06-14 16:47:31 -0700

    First pass at using STM for global state

--- Main.hs
+++ Main.hs
@@ -1,10 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Control.Concurrent.STM
+import Control.Monad.IO.Class
+import Data.Text.Lazy
+import Data.Monoid
 import Web.Scotty
 
-import Data.Monoid (mconcat)
-
 main = scotty 3000 $ do
+  counterT <- liftIO $ newTVarIO 0
   get "/:word" $ do
     beam <- param "word"
-    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+    times <- liftIO $ atomically $ do
+      current <- readTVar counterT
+      swapTVar counterT (current + 1)
+    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1><h2>", pack (show times), "</h2>"]
