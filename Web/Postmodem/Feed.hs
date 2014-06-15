cf0b2d0947dffdaee0fb740db34b25c6f02167e0
Colin Barrett <colin@springsandstruts.com>
2014-06-14 23:32:03 -0700

    Port Feed module

---
+++ Web/Postmodem/Feed.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE NamedFieldPuns, ViewPatterns #-}
+
+module Web.Postmodem.Feed
+  ( getEpisodes
+  , Episode
+  , Audio
+  ) where
+
+import Data.Maybe
+import Data.Time.Clock
+import Network.Curl.Download
+import Text.Feed.Query
+import Text.Feed.Types
+
+data Episode = Episode
+  { title :: String
+  , description :: String
+  , date :: UTCTime
+  , audio :: Audio
+  } deriving (Show)
+
+data Audio = Audio
+  { url :: String
+  , _type :: String
+  } deriving (Show)
+
+getEpisodes :: IO (Maybe [Episode])
+getEpisodes = openAsFeed "http://feeds.feedburner.com/postmodem" >>= return . either (const Nothing) (Just . processFeed)
+
+processFeed :: Feed -> [Episode]
+processFeed f = catMaybes $ for (feedItems f) $ \item -> do
+  title <- getItemTitle item
+  description <- getItemDescription item
+  Just date <- getItemPublishDate item
+  (audioURL, Just audioType, _) <- getItemEnclosure item
+  return Episode { title, description, date, audio = Audio { url = audioURL, _type = audioType } }
+
+for = flip fmap
