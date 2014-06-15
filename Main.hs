1a7cf7432959a5550d7a2bc74b71bd6d56f6ce80
Colin Barrett <colin@springsandstruts.com>
2014-06-14 17:31:27 -0700

    Embed a reader monad in Scotty's monads so we can access our TVars from anywhere without lexical capturing. Wheee.

--- Main.hs
+++ Main.hs
@@ -2,15 +2,31 @@
 
 import Control.Concurrent.STM
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Reader
 import Data.Text.Lazy
 import Data.Monoid
-import Web.Scotty
+import Web.Scotty.Trans
 
-main = scotty 3000 $ do
-  counterT <- liftIO $ newTVarIO 0
-  get "/:word" $ do
-    beam <- param "word"
-    times <- liftIO $ atomically $ do
-      current <- readTVar counterT
-      swapTVar counterT (current + 1)
-    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1><h2>", pack (show times), "</h2>"]
+data AppState = AppState {
+  counterT :: TVar Int
+}
+type AppActionM a = ActionT Text (ReaderT AppState IO) a
+askCounterT = lift $ asks counterT
+
+main = do
+  counterT <- newTVarIO 0
+  let initialState = AppState { counterT = counterT }
+      runAppT r = runReaderT r initialState
+  scottyT 3000 runAppT runAppT $ do
+    get "/:word" getWord
+
+getWord :: AppActionM ()
+getWord = do
+  counterT <- askCounterT
+  beam <- param "word"
+  times <- liftIO $ atomically $ do
+    current <- readTVar counterT
+    swapTVar counterT (current + 1)
+  html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1><h2>", pack (show times), "</h2>"]
+  
